a03fec7ccd377a6f37b8d039c877d1ad310ee2ea
Dominik <kxmischesdomi@gmail.com>
2025-12-02 23:21:30 +0100

    feat: preview

--- src/client/components/board/card/Card.tsx
+++ src/client/components/board/card/Card.tsx
@@ -1,6 +1,7 @@
 import './Card.css';
 import back from '/assets/cards/back/0.jpg';
-import { CSSProperties } from 'react';
+import { CSSProperties, useState } from 'react';
+import { createPortal } from 'react-dom';
 import { Card as CardType } from "../../../../common";
 
 interface CardProps {
@@ -13,17 +14,36 @@ interface CardProps {
 }
 
 export default function Card({ card, index, count, angle, offsetX, offsetY,  }: CardProps) {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const cardImage = card ? `/assets/cards/${card.name}/${card.index}.png` : back;
+
   return (
-    <div
-      className="card"
-      style={{
-        backgroundImage: `url(${card ? `/assets/cards/${card.name}/${card.index}.png` : back})`,
-        position: 'absolute',
-        '--base-transform': `translate(${offsetX}%, ${offsetY}%) rotate(${angle}deg)`,
-        transformOrigin: 'center 200%',
-        zIndex: count - index,
-      } as CSSProperties}
-    />
+    <>
+      <div
+        className="card"
+        style={{
+          backgroundImage: `url(${cardImage})`,
+          position: 'absolute',
+          '--base-transform': `translate(${offsetX}%, ${offsetY}%) rotate(${angle}deg)`,
+          transformOrigin: 'center 200%',
+          zIndex: count - index,
+        } as CSSProperties}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      />
+      {isHovered && card && createPortal(
+        <div className="card-preview-overlay">
+          <div
+            className="card-preview"
+            style={{
+              backgroundImage: `url(${cardImage})`,
+            } as CSSProperties}
+          />
+        </div>,
+        document.body
+      )}
+    </>
   );
 }
 
